6e5b1ab9b77d36fe221a75d30d2c02ca07604e3f
Adrian Kaluzinski <kaluza@gmail.com>
2021-08-26 15:55:48 +0200

    Create cart context

--- ui/src/components/Layout/HeaderCardButton/HeaderCardButton.js
+++ ui/src/components/Layout/HeaderCardButton/HeaderCardButton.js
@@ -1,13 +1,20 @@
 import classes from "./HeaderCardButton.module.css";
-
-import CartIcon from '../../Cart/CartIcon/CartIcon';
+import { useContext } from "react";
+import CartIcon from "../../Cart/CartIcon/CartIcon";
+import CartContext from "../../../store/cart-context";
 
 const HeaderCardButton = (props) => {
+
+  const cartCtx = useContext(CartContext);
+  const cartItems = cartCtx.items.reduce((curr, item) => { return curr + item.amount }, 0);
+
   return (
     <button className={classes.button} onClick={props.onClick}>
-      <span className={classes.icon}><CartIcon></CartIcon></span>
+      <span className={classes.icon}>
+        <CartIcon></CartIcon>
+      </span>
       <span>Your Cart</span>
-      <span className={classes.badge}>5</span>
+      <span className={classes.badge}>{cartItems}</span>
     </button>
   );
 };
