3889143d87eb291eba90d134fcadc476ddfdd77f
Adrian Kaluzinski <kaluza@gmail.com>
2021-10-21 14:48:55 +0200

    Change list of available meals to class-based component

--- ui/src/components/Meals/AvailableMeals/AvailableMeals.js
+++ ui/src/components/Meals/AvailableMeals/AvailableMeals.js
@@ -2,7 +2,33 @@ import { DUMMY_MEALS } from "../../../fixtures/dummy-meals";
 import Card from "../../shared/Card/Card";
 import classes from "./AvailableMeals.module.css";
 import MealItem from "./MealItem/MealItem";
+import { Component} from 'react'
+  
+class AvailableMeals extends Component {
 
+  render() {
+    const meals = DUMMY_MEALS.map((meal) => (
+      <MealItem
+        id={meal.id}
+        key={meal.id}
+        name={meal.name}
+        price={meal.price}
+        description={meal.description}
+      />
+    ));
+  
+    return (
+      <section className={classes.meals}>
+        <Card>
+          <ul>{meals}</ul>
+        </Card>
+      </section>
+    );
+  }
+
+}
+
+/*
 const AvailableMeals = () => {
   const meals = DUMMY_MEALS.map((meal) => (
     <MealItem
@@ -22,5 +48,5 @@ const AvailableMeals = () => {
     </section>
   );
 };
-
+*/
 export default AvailableMeals;
