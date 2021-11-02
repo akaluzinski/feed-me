93c29df973a0907aae5f102836d09045fd41b091
Adrian Kaluzinski <kaluza@gmail.com>
2021-11-02 19:55:08 +0100

    Display error message on loading/error

--- ui/src/components/Meals/AvailableMeals/AvailableMeals.js
+++ ui/src/components/Meals/AvailableMeals/AvailableMeals.js
@@ -2,56 +2,68 @@ import Card from "../../shared/Card/Card";
 import classes from "./AvailableMeals.module.css";
 import MealItem from "./MealItem/MealItem";
 import { useEffect, useState } from "react";
-import { testURL } from '../../../fixtures/test-data';
+import { testURL } from "../../../fixtures/test-data";
 
 const AvailableMeals = () => {
+  const [meals, setMeals] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [isError, setIsError] = useState(false);
 
-    const [meals, setMeals] = useState([])
+  useEffect(() => {
+    const fetchMeals = async () => {
+      const response = await fetch(testURL);
+      console.log(response);
+      const data = await response.json();
 
-    useEffect(() => {
-      const fetchMeals = async () => {
-        const response = await fetch(testURL);
-        console.log(response);
-        const data = await response.json();
+      if (data?.error === "Permission denied") {
+        setIsError(true);
+        setIsLoading(false);
+        return;
+      }
 
-        if(data?.error === 'Permission denied') {
-          console.error('Permission denied. Verify DB connection');
-          return;
-        }
+      const loadedMeals = [];
 
-        const loadedMeals = [];
+      for (const key in data) {
+        loadedMeals.push({
+          id: key,
+          name: data[key].name,
+          price: data[key].price,
+        });
+      }
 
-        for (const key in data) {
-          loadedMeals.push({
-            id: key,
-            name: data[key].name,
-            price: data[key].price
-          })
-        }
+      setMeals(loadedMeals);
+      setIsLoading(false);
+    };
 
-        setMeals(loadedMeals);
-      };
+    fetchMeals();
+  }, []);
 
-      fetchMeals();
-    }, []);
-
-    const mealsList = meals.map((meal) => (
-      <MealItem
-        id={meal.id}
-        key={meal.id}
-        name={meal.name}
-        price={meal.price}
-        description={meal.description}
-      />
-    ));
-
+  if (isError || isLoading) {
     return (
-      <section className={classes.meals}>
-        <Card>
-          <ul>{mealsList}</ul>
-        </Card>
+      <section className={classes.mealIsLoading}>
+        { isLoading && <p>Loading...</p> }
+        { isError && <p>Error :(</p> }
       </section>
     );
   }
+
+  const mealsList = meals.map((meal) => (
+    <MealItem
+      id={meal.id}
+      key={meal.id}
+      name={meal.name}
+      price={meal.price}
+      description={meal.description}
+    />
+  ));
+
+  return (
+    <section className={classes.meals}>
+      <Card>
+        <ul>{mealsList}</ul>
+      </Card>
+    </section>
+  );
+};
 
 export default AvailableMeals;
