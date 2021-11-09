5ce85e5c2ba9de8b6a9b5810495ff7a2e0a9bbc3
Adrian Kaluzinski <kaluza@gmail.com>
2021-11-09 18:36:06 +0100

    Update styles in checkout

--- ui/src/components/Cart/Checkout.js
+++ ui/src/components/Cart/Checkout.js
@@ -6,27 +6,29 @@ const Checkout = (props) => {
   };
 
   return (
-    <form onSubmit={confirmHandler}>
+    <form className={classes.form} onSubmit={confirmHandler}>
       <div className={classes.control}>
-        <label htmlFor="name">Your name</label>
-        <input type="text" id="name"></input>
+        <label htmlFor="name">Your Name</label>
+        <input type="text" id="name" />
       </div>
       <div className={classes.control}>
         <label htmlFor="street">Street</label>
-        <input type="text" id="street"></input>
+        <input type="text" id="street" />
       </div>
       <div className={classes.control}>
         <label htmlFor="postal">Postal Code</label>
-        <input type="text" id="postal"></input>
+        <input type="text" id="postal" />
       </div>
       <div className={classes.control}>
         <label htmlFor="city">City</label>
-        <input type="text" id="city"></input>
+        <input type="text" id="city" />
       </div>
-      <button type="button" onClick={props.onCancel}>
-        Cancel
-      </button>
-      <button>Confirm</button>
+      <div className={classes.actions}>
+        <button type="button" onClick={props.onCancel}>
+          Cancel
+        </button>
+        <button className={classes.submit}>Confirm</button>
+      </div>
     </form>
   );
 };
